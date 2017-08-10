db7cc09361783cf7ab94ac0c0ddaa5e98613de3c
seagull <crab.co@qq.com>
2017-08-10 14:54:46 +0800

    创建标准定义并准备开始开发功能

---
+++ BigRec/inc/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+define("DATABASE_HOST", "localhost");					//数据库地址
+define("DATABASE_USER", "big_user");				   	//数据库帐号
+define("DATABASE_PASS", "big_pass");				    //数据库密码
+define("DATABASE_NAME", "gli_bigrecommend");			//数据库名称
+
+
+
+$SelfPath				= substr(dirname(__FILE__),0,-4);
+//系统根目录  
+define('SYSTEM_PATH',	$SelfPath); 
